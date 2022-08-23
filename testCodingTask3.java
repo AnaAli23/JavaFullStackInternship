98ec1cfa4597550c914b0202eae9dd6b8a32b765
AnaAli23 <76102117+AnaAli23@users.noreply.github.com>
2022-08-23 19:37:44 +0530

    Add files via upload

---
+++ testCodingTask3.java
@@ -0,0 +1,24 @@
+package CollectionsPrograms;
+
+import java.util.HashSet;
+
+public class testCodingTask3 {
+
+	public static void main(String[] args) {
+		HashSet<Integer> set = new HashSet<>();
+		
+		set.add(100);
+		set.add(500);
+		set.add(700);
+		set.add(400);
+		set.add(300);
+		set.add(800);
+		set.add(600);
+		set.add(800);
+		set.add(700);
+		
+		System.out.println("Set contains elements as follows: "+set);
+
+	}
+
+}
