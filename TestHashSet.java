e0c412dd7e79242a209c7f469fc2265d09aadee1
AnaAli23 <76102117+AnaAli23@users.noreply.github.com>
2022-08-23 18:03:26 +0530

    Add files via upload

---
+++ TestHashSet.java
@@ -0,0 +1,18 @@
+package CollectionsPrograms;
+
+import java.util.HashSet;
+
+public class TestHashSet {
+
+	public static void main(String[] args) {
+		HashSet<Integer> set = new HashSet<>();
+		set.add(8);
+		set.add(2);
+		set.add(1);
+		set.add(4);
+		set.add(5);
+		set.add(3);
+		System.out.println("Set contains Group of Integer elements : "+set);
+	}
+
+}
